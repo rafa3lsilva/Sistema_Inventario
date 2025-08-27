9117ab7ef4a1c4d90587e2c428ab7dd5e5aac89d
rafael <rafaelpjmp@gmail.com>
2025-08-27 20:21:31 -0300

    ajustando hora sidebar_admin

--- sidebar_admin.py
+++ sidebar_admin.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import datetime
 from PIL import Image
+import pytz
 from database_api import get_all_users
 
 
@@ -24,12 +25,17 @@ def admin_sidebar(username):
     except:
         st.sidebar.markdown("🧑‍💼")
 
+    # 2. Definimos o fuso horário de São Paulo (que representa o horário de Brasília)
+    sao_paulo_tz = pytz.timezone("America/Sao_Paulo")
+
+    # 3. Obtemos a hora atual JÁ com o fuso horário correto
+    hora_local = datetime.now(sao_paulo_tz)
     # Informações do usuário
     st.sidebar.markdown(f"""
 **👋 Bem-vindo, `{username}`**
 
 🔐 Função: `Administrador`  
-🕒 Sessão: `{datetime.now().strftime('%d/%m/%Y %H:%M')}`
+🕒 Sessão: `{hora_local.strftime('%d/%m/%Y %H:%M')}`
 """)
 
     st.sidebar.markdown("---")
