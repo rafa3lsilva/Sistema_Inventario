1c5a8e6f853b982eff56b9a1166cddf346fb1f54
rafael <rafaelpjmp@gmail.com>
2025-08-29 11:58:26 -0300

    inserindo msg para usuario apos cadastro

--- modules/page_cadastro.py
+++ modules/page_cadastro.py
@@ -17,6 +17,9 @@ def show_cadastro(set_page):
                 st.error(f"Erro no cadastro: {res}")
             elif res and res.user:
                 st.success("Conta criada! Por favor, faça o login.")
+                st.info(
+                    "✉️ Por favor, verifique a sua caixa de entrada (e spam) " \
+                    "para ativar a sua conta antes de fazer o login.")
                 set_page("login")
             else:
                 st.error(
